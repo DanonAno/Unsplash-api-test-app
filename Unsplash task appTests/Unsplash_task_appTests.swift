8bedb0660024190dc0cce0b70add904401de8345
Daniil Luparevich <daniilluparevich@gmail.com>
2022-01-30 16:04:48 +0500

    Initial Commit

---
+++ Unsplash task appTests/Unsplash_task_appTests.swift
@@ -0,0 +1,33 @@
+//
+//  Unsplash_task_appTests.swift
+//  Unsplash task appTests
+//
+//  Created by  Даниил on 30.01.2022.
+//
+
+import XCTest
+@testable import Unsplash_task_app
+
+class Unsplash_task_appTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
